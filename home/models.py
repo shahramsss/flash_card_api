f55db8953d3dbffb14a4b7451c6e774f18d0e855
shahramsss <shahramsss1987@gmail.com>
2025-01-25 17:12:51 +0330

    add model falash card

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class FlashCard(models.Model):
+    word = models.CharField(max_length=512)
+    meaning = models.CharField(max_length=512)
+    example = models.CharField(max_length=1024, null=True, blank=True)
+    rate = models.SmallIntegerField(default=0)
+    last_reply = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    next_review_date = models.DateField()
