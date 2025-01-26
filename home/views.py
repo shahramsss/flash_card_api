73f8d5074772f61dc2451ffb12666a0e7860db4e
shahramsss <shahramsss1987@gmail.com>
2025-01-26 12:10:21 +0330

    dont

--- home/views.py
+++ home/views.py
@@ -36,13 +36,6 @@ class Word(APIView):
         serializer = FlashCardSerializer(flashcard)
         return Response(data=serializer.data)
     
-    def post(self, request):
-        serializer = FlashCardSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
     def update(self, request , pk ):
         pass
 
